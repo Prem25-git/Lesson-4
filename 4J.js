e90ad00ef862909e0d2a5761c041f3cbdf07b04b
Premkumar <premkumar.zangroups@gmail.com>
2024-11-13 16:08:10 +0530

    lesson4

---
+++ 4J.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      .add{background-color: yellow;}
+      .buy{background-color: orange;}
+
+    </style>
+
+</head>
+  <body>
+    <p>Adult s Pian Cotton T-shirt</p>
+    <p>Price : $7.99</p>
+
+    <button class = "add" onclick="alert('Added');">Add to cart</button>
+    <button class = "buy" onclick="alert('Loading...'); alert('Purchased');">Buy now
+      </button>
+  </body>
+  </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
