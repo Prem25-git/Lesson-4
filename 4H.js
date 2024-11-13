2d2a1be1bea7e71b5a38ef5147ce89a9646eb9ad
Premkumar <premkumar.zangroups@gmail.com>
2024-11-13 16:05:42 +0530

    Lesson4

---
+++ 4H.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <title>ZanGroups</title>
+  <head>
+    <style>
+      .cart{background-color: yellow;}
+      .buy{background-color: orange;}
+
+    </style>
+    <body>
+      <button>Click</button>
+      <button>Premkumar</button>
+      <button class="cart">Add to cart</button>
+      <button class="buy">Buy Now</button>
+    
+    </body>
+    <p>Hello Premkumar </p>
+    <p>Hello, World ! <br> Today i learned HTML , CSS, Javascript concepts </p>
+    
+     <script>
+      console.log("Welcome Zangroup web page");
+     </script>
+
+  </head>
+
+</html>
