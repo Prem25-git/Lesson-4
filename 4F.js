ec0f6ca9981565a8c56f3e1da49c5f59c95cf76c
Premkumar <premkumar.zangroups@gmail.com>
2024-11-13 16:03:30 +0530

    Lesson4

---
+++ 4F.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      .cart{background-color: yellow;}
+      .buy{background-color: orange;}
+
+    </style>
+    <body>
+      <button>Click</button>
+      <button>Premkumar</button>
+      <button class="cart">Add to cart</button>
+      <button class="buy">Buy Now</button>
+    
+    </body>
+    <p>Hello Premkumar </p>
+    <p>Hello, World ! <br> Today i learned HTML , CSS, Javascript concepts </p>
+
+  </head>
+
+</html>
